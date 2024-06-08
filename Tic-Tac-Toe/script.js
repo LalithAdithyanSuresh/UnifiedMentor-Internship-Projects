123ae4c12ff02ef783afa4ac5d82316c9f7ce53e
Lalith Adithyan Suresh <140822743+LalithAdithyanSuresh@users.noreply.github.com>
2024-06-08 18:00:22 +0530

    Documentation Done

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -1,9 +1,11 @@
-
+// Initiating variables
 let grid = [[0,0,0],[0,0,0],[0,0,0]];
 let player = 'O';
 play = true;
 
+// Function to Check for win conditions every play
 function WinConditionCheck(){
+    // Horizontal Check
     for(i=0;i<3;i++){
         f=1;
         for(j=0;j<3;j++){
@@ -20,6 +22,7 @@ function WinConditionCheck(){
             }
         }
     }
+    // Vertical Check
     for(i=0;i<3;i++){
         f=1;
         for(j=0;j<3;j++){
@@ -36,6 +39,7 @@ function WinConditionCheck(){
             }
         }
     }
+    // First Diagonal Check
     f=1;
     for(i=0;i<3;i++){
         if(grid[i][i] != player){
@@ -50,6 +54,7 @@ function WinConditionCheck(){
             document.getElementById("RestartBtn").classList.add('RestartBtnAnim');
         }
     }
+    // Second Diagonal Check
     f=1;
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
@@ -71,6 +76,7 @@ function WinConditionCheck(){
     
 }
 
+// Function to change the player every play
 function ChangePlayer(){
     if(player == "X"){
         player = "O";
@@ -87,11 +93,14 @@ function ChangePlayer(){
     }
 }
 
+// Function to acknowledge player input and update accordingly
 function SquareClick(id){
     if(grid[Math.floor((id-1)/3)][(id-1)%3] == 0 && play==true){
+        // Adding visual sprite on the board
         Elem = document.createElement("div");
         Elem.classList.add(player);
         document.getElementById("Square"+id).appendChild(Elem);
+        // Updating Matrix
         grid[Math.floor((id-1)/3)][(id-1)%3]= player;
         WinConditionCheck();
         if(play==true){
@@ -100,5 +109,5 @@ function SquareClick(id){
     }
 }
 
-
+// Intial Player Setup
 ChangePlayer();
